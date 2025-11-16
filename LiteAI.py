e0a4372e604de8f7705035392ffa23f872e4c1fe
Rea1410 <kevinreagan2012@gmail.com>
2025-11-16 18:45:42 +0700

    Lite AI from my youtube tutorial

---
+++ LiteAI.py
@@ -0,0 +1,25 @@
+#how to make a lttle ai in python
+
+#optional
+print("AI 1.0")
+print("Hello!")
+
+#message
+message=input("= ")
+
+#what AI will answer if you ask
+if ("hello") in message or ("hi") in message:
+    print("hello!")
+elif ("what is") in message and ("youtube") in message:
+    print("Youtube is a online video platform")
+elif ("subscribe") in message:
+    print("subscribe and like!")
+elif ("what is")in message and ("roblox") in message:
+    print("Roblox is a online game platform")
+elif ("who are you") in message:
+    print("I am lite AI 1.0")
+elif ("bye") in message or ("goodbye") in message:
+    print("goodbye!")
+else:
+    print("Sorry, idk that")
+input("Thx For Using This AI, ENTER To Close")
